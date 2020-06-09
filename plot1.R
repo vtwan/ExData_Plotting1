d7e3daefb0cb5ed0a51e5a519cebb98ab3f83cea
vtwan <35957691+vtwan@users.noreply.github.com>
2020-06-09 16:46:01 +0700

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,20 @@
+## Getting full dataset
+ig <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+               nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+ig$Date <- as.Date(ig$Date, format="%d/%m/%Y")
+
+## Subsetting the data
+meow <- subset(ig, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(ig)
+
+## Converting dates
+hello_ig <- paste(as.Date(meow$Date), meow$Time)
+meow$Datetime <- as.POSIXct(hello_ig)
+
+## Plot 1
+hist(meow$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+## Saving to file
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
